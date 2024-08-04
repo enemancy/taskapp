cce98a03884e929c7bd42816fd3cda95ec3dd758
root <root@DESKTOP-0LA4ALD>
2024-08-04 21:02:08 +0900

    mytask only belongTo creater_id

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Todo extends Model
 {
     use HasFactory;
+
+    public static function getCurrentUserTodos(){
+        return Todo::where('creater_id', Auth::user()->id)->get();
+    }
 }
